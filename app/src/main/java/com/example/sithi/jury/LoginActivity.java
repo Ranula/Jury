5fa10ff6a0bc087770c41953fe920cf08d7b83e0
sithija piyuman <sithija piyuman>
2017-10-07 14:29:35 +0530

    initial commit

---
+++ app/src/main/java/com/example/sithi/jury/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.sithi.jury;
+
+//import android.app.ProgressDialog;
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.util.Log;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.content.Intent;
+//import android.view.View;
+//import android.widget.Toast;
+//import com.android.volley.Request;
+//import com.android.volley.Response;
+//import com.android.volley.VolleyError;
+//import com.android.volley.toolbox.StringRequest;
+//import org.json.JSONException;
+//import org.json.JSONObject;
+//import java.util.HashMap;
+//import java.util.Map;
+/**
+ * Created by sithi on 10/7/2017.
+ */
+
+public class LoginActivity {
+
+
+}
